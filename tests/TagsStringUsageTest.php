87b94b42d98a805340f0edd74b6624510e4435a4
Martin Halaj <m.halaj@websystem.sk>
2022-07-16 20:46:35 +0200

    start testing

---
+++ tests/TagsStringUsageTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class TagsStringUsageTest extends TestCase
+{
+    /** @test */
+    public function true_equals_true()
+    {
+        return $this->assertTrue(true);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,13 +1,12 @@
 <?php
 
-use Websystem\Tags\Providers\TagsServiceProvider;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function getPackageProviders($app)
     {
         return [
-            TagsServiceProvider::class,
+            Websystem\Tags\TagServiceProvider::class,
         ];
     }
 
